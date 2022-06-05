9fb3c4d15f442b73c3fedf50d343ad724bf7f712
vladbakhteev <bakhteev_v@mail.ru>
2022-06-05 11:47:37 +0200

    Refactor

--- 03-orchestration/homework.py
+++ 03-orchestration/homework.py
@@ -4,9 +4,11 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+
 def read_data(path):
     df = pd.read_parquet(path)
     return df
+
 
 def prepare_features(df, categorical, train=True):
     df['duration'] = df.dropOff_datetime - df.pickup_datetime
@@ -18,15 +20,15 @@ def prepare_features(df, categorical, train=True):
         print(f"The mean duration of training is {mean_duration}")
     else:
         print(f"The mean duration of validation is {mean_duration}")
-    
+
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
     return df
 
-def train_model(df, categorical):
 
+def train_model(df, categorical):
     train_dicts = df[categorical].to_dict(orient='records')
     dv = DictVectorizer()
-    X_train = dv.fit_transform(train_dicts) 
+    X_train = dv.fit_transform(train_dicts)
     y_train = df.duration.values
 
     print(f"The shape of X_train is {X_train.shape}")
@@ -39,9 +41,10 @@ def train_model(df, categorical):
     print(f"The MSE of training is: {mse}")
     return lr, dv
 
+
 def run_model(df, categorical, dv, lr):
     val_dicts = df[categorical].to_dict(orient='records')
-    X_val = dv.transform(val_dicts) 
+    X_val = dv.transform(val_dicts)
     y_pred = lr.predict(X_val)
     y_val = df.duration.values
 
@@ -49,9 +52,9 @@ def run_model(df, categorical, dv, lr):
     print(f"The MSE of validation is: {mse}")
     return
 
-def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
-           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
 
+def main(train_path: str = './data/fhv_tripdata_2021-01.parquet',
+         val_path: str = './data/fhv_tripdata_2021-02.parquet'):
     categorical = ['PUlocationID', 'DOlocationID']
 
     df_train = read_data(train_path)
@@ -63,5 +66,6 @@ def main(train_path: str = './data/fhv_tripdata_2021-01.parquet',
     # train the model
     lr, dv = train_model(df_train_processed, categorical)
     run_model(df_val_processed, categorical, dv, lr)
+
 
 main()
